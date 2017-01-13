8684465cbeec59736ed004c2d1587809a7ce0fb3
Casper Björk <erik.casper.bjork@gmail.com>
2017-01-13 14:06:34 +0100

    DB Updated

--- kompis_kak.sql
+++ kompis_kak.sql
@@ -3,9 +3,9 @@
 -- http://www.phpmyadmin.net
 --
 -- Värd: 127.0.0.1
--- Tid vid skapande: 09 jan 2017 kl 19:07
--- Serverversion: 10.1.13-MariaDB
--- PHP-version: 5.6.23
+-- Tid vid skapande: 13 jan 2017 kl 14:04
+-- Serverversion: 10.1.16-MariaDB
+-- PHP-version: 5.6.24
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -17,7 +17,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Databas: `matforalla`
+-- Databas: `kompis_kak`
 --
 
 -- --------------------------------------------------------
@@ -41,6 +41,30 @@ INSERT INTO `favorittag` (`ID`, `Tag_ID`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Ersättningsstruktur för vy `infoingrediense`
+--
+CREATE TABLE `infoingrediense` (
+`recept_id` int(11)
+,`name` varchar(75)
+,`amount` varchar(20)
+);
+
+-- --------------------------------------------------------
+
+--
+-- Ersättningsstruktur för vy `inforecept`
+--
+CREATE TABLE `inforecept` (
+`ID` int(11)
+,`ReceptNamn` varchar(75)
+,`description` varchar(150)
+,`TagNamn` varchar(45)
+,`Author` varchar(45)
+);
+
+-- --------------------------------------------------------
+
+--
 -- Tabellstruktur `ingrediense`
 --
 
@@ -48,6 +72,14 @@ CREATE TABLE `ingrediense` (
   `ID` int(11) NOT NULL,
   `name` varchar(75) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumpning av Data i tabell `ingrediense`
+--
+
+INSERT INTO `ingrediense` (`ID`, `name`) VALUES
+(1, 'Socker'),
+(2, 'Smör');
 
 -- --------------------------------------------------------
 
@@ -60,8 +92,7 @@ CREATE TABLE `recept` (
   `name` varchar(75) NOT NULL,
   `description` varchar(150) NOT NULL,
   `author_id` int(11) NOT NULL,
-  `tag_id` int(11) NOT NULL,
-  `col_id` int(11) NOT NULL
+  `tag_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -71,10 +102,18 @@ CREATE TABLE `recept` (
 --
 
 CREATE TABLE `recept_collective` (
-  `col_id` int(11) NOT NULL,
+  `recept_id` int(11) NOT NULL,
   `amount` varchar(20) NOT NULL,
   `ing_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumpning av Data i tabell `recept_collective`
+--
+
+INSERT INTO `recept_collective` (`recept_id`, `amount`, `ing_id`) VALUES
+(1, '2 tsk', 1),
+(1, '2 liter', 2);
 
 -- --------------------------------------------------------
 
@@ -84,9 +123,16 @@ CREATE TABLE `recept_collective` (
 
 CREATE TABLE `tag` (
   `ID` int(11) NOT NULL,
-  `name` int(11) NOT NULL,
+  `name` varchar(45) NOT NULL,
   `description` varchar(150) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumpning av Data i tabell `tag`
+--
+
+INSERT INTO `tag` (`ID`, `name`, `description`) VALUES
+(1, 'Efterrätt', 'Det bästa i en middag');
 
 -- --------------------------------------------------------
 
@@ -107,6 +153,24 @@ CREATE TABLE `user` (
 
 INSERT INTO `user` (`ID`, `Name`, `Privileges`, `Fav_ID`) VALUES
 (1, 'Casper Bjork', 3, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur för vy `infoingrediense`
+--
+DROP TABLE IF EXISTS `infoingrediense`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `infoingrediense`  AS  select `recept_collective`.`recept_id` AS `recept_id`,`ingrediense`.`name` AS `name`,`recept_collective`.`amount` AS `amount` from (`recept_collective` join `ingrediense`) where (`ingrediense`.`ID` = `recept_collective`.`ing_id`) ;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur för vy `inforecept`
+--
+DROP TABLE IF EXISTS `inforecept`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `inforecept`  AS  select `recept`.`ID` AS `ID`,`recept`.`name` AS `ReceptNamn`,`recept`.`description` AS `description`,`tag`.`name` AS `TagNamn`,`user`.`Name` AS `Author` from ((`recept` join `tag`) join `user`) where ((`tag`.`ID` = `recept`.`tag_id`) and (`user`.`ID` = `recept`.`author_id`)) ;
 
 --
 -- Index för dumpade tabeller
@@ -155,17 +219,17 @@ ALTER TABLE `favorittag`
 -- AUTO_INCREMENT för tabell `ingrediense`
 --
 ALTER TABLE `ingrediense`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT för tabell `recept`
 --
 ALTER TABLE `recept`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT för tabell `tag`
 --
 ALTER TABLE `tag`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT för tabell `user`
 --
