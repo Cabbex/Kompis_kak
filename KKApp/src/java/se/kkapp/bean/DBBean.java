4ae6303163ea88de6e97443cf760ec943182f3e7
Casper Björk <erik.casper.bjork@gmail.com>
2017-01-13 17:50:51 +0100

    Added POST and PUT, fixed DELETE
    All REST functions working now. More to add before Back-end is ready for
    Front-end use

--- KKApp/src/java/se/kkapp/bean/DBBean.java
+++ KKApp/src/java/se/kkapp/bean/DBBean.java
@@ -1,18 +1,19 @@
-
 package se.kkapp.bean;
 
 import com.mysql.jdbc.Connection;
+import java.io.StringReader;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.ejb.Stateless;
 import javax.json.JsonArray;
 import se.kkapp.support.ConnectionFactory;
 import javax.inject.Named;
 import javax.json.Json;
 import javax.json.JsonArrayBuilder;
+import javax.json.JsonObject;
+import javax.json.JsonReader;
+
 /**
  *
  * @author Casper
@@ -28,10 +29,10 @@ public class DBBean {
             String sql = "SELECT * FROM recept";
             ResultSet data = stmt.executeQuery(sql);
             JsonArrayBuilder JAB = Json.createArrayBuilder();
-            while(data.next()){
+            while (data.next()) {
                 String name = data.getString("name");
                 String desc = data.getString("description");
-                
+
                 JAB.add(Json.createObjectBuilder()
                         .add("name", name)
                         .add("desc", desc));
@@ -39,7 +40,7 @@ public class DBBean {
             connection.close();
             return JAB.build();
         } catch (Exception e) {
-            System.out.println("Fail på getRecept: "+ e);
+            System.out.println("Fail på getRecept: " + e);
         }
         return null;
     }
@@ -49,10 +50,72 @@ public class DBBean {
             Connection connection = ConnectionFactory.createConnection();
             PreparedStatement stmt = connection.prepareStatement("DELETE FROM recept WHERE recept.ID = ?");
             stmt.setInt(1, id);
+            stmt.executeUpdate();
+            stmt = connection.prepareStatement("DELETE FROM `recept_collective` WHERE recept_collective.recept_id = ?");
+            stmt.setInt(1, id);
+            stmt.executeUpdate();
             connection.close();
             return true;
         } catch (Exception ex) {
-            System.out.println("RemoveRecept error: "+ex);
+            System.out.println("RemoveRecept error: " + ex);
+        }
+        return false;
+    }
+
+    /*
+        Used for testing!
+        [{
+            "rname": "Pannkaka",
+            "description": "mm pannkaka",
+            "author": "Casper Bjork",
+            "tag": "Efterrätt"
+        }]
+     */
+    public boolean postRecept(String body) {
+        JsonReader jsr = Json.createReader(new StringReader(body));
+        JsonArray jsondata = jsr.readArray();
+        JsonObject jsono = jsondata.getJsonObject(0);
+        String rname = jsono.getString("rname");
+        String desc = jsono.getString("description");
+        String author = jsono.getString("author");
+        String tag = jsono.getString("tag");
+        try {
+            Connection connection = ConnectionFactory.createConnection();
+            PreparedStatement stmt = connection.prepareStatement("INSERT INTO `recept`(`ID`, `name`, `description`, `author_id`, `tag_id`) "
+                    + "VALUES (null,?,?,(SELECT user.ID FROM user WHERE user.Name = " + "'" + author + "'" + "),(SELECT tag.ID FROM tag WHERE tag.name = " + "'" + tag + "'" + "))");
+            stmt.setString(1, rname);
+            stmt.setString(2, desc);
+            stmt.executeUpdate();
+            connection.close();
+            return true;
+        } catch (Exception ex) {
+            System.out.println("POSTRecept error: " + ex);
+        }
+        return false;
+    }
+
+    public boolean putRecept(String body, int id) {
+        JsonReader jsr = Json.createReader(new StringReader(body));
+        JsonArray jsondata = jsr.readArray();
+        JsonObject jsono = jsondata.getJsonObject(0);
+        String rname = jsono.getString("rname");
+        String desc = jsono.getString("description");
+        String author = jsono.getString("author");
+        String tag = jsono.getString("tag");
+        try {
+            Connection connection = ConnectionFactory.createConnection();
+            PreparedStatement stmt = connection.prepareStatement("UPDATE `recept` SET `ID`=?,`name`= ?,`description`=?,"
+                    + "`author_id`= (SELECT user.ID FROM user WHERE user.Name = " + "'" + author + "'" + ") ,"
+                    + "`tag_id`= (SELECT tag.ID FROM tag WHERE tag.name = " + "'" + tag + "'" + ") WHERE recept.ID = ?");
+            stmt.setInt(1, id);
+            stmt.setString(2, rname);
+            stmt.setString(3, desc);
+            stmt.setInt(4, id);
+            stmt.executeUpdate();
+            connection.close();
+            return true;
+        } catch (Exception ex) {
+            System.out.println("PUTRecept error: " + ex);
         }
         return false;
     }
--- KKApp/src/java/se/kkapp/service/Webservice.java
+++ KKApp/src/java/se/kkapp/service/Webservice.java
@@ -7,8 +7,11 @@ package se.kkapp.service;
 
 import javax.ejb.EJB;
 import javax.json.JsonArray;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -45,6 +48,25 @@ public class Webservice {
     @Path("/recept/{id}")
     public Response removeRecept(@PathParam("id") int id){
         if (!DBB.removeRecept(id)){
+            return Response.serverError().build();
+        }
+        return Response.ok().build();
+    }
+    
+    @POST
+    @Path("/recept")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response postRecept(String body){
+        if(!DBB.postRecept(body)){
+            return Response.serverError().build();
+        }
+        return Response.ok().build();
+    }
+    
+    @PUT
+    @Path("/recept/{id}")
+    public Response putRecept(String body, @PathParam("id") int id){
+        if(!DBB.putRecept(body, id)){
             return Response.serverError().build();
         }
         return Response.ok().build();
