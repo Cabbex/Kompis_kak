00754a85fe09d7e962acccb3b966a2473dc45c9d
Casper Björk <erik.casper.bjork@gmail.com>
2017-01-19 14:22:31 +0100

    Fixed frontpage begin to add login

--- KKClient/public_html/JS/KKApp.js
+++ KKClient/public_html/JS/KKApp.js
@@ -1,25 +1,38 @@
 var module = angular.module("KKApp", ["ui.router"]);
 
-module.config(function ($urlRouterProvider, $stateProvider){
-   $urlRouterProvider.otherwise("/");
-   
-   $stateProvider.state("home",{
-       url:"/",
-       templateUrl:"templates/home.html",
-       controller:"homeCtrl"
-   });
-});
+module.config(function ($urlRouterProvider, $stateProvider) {
+    $urlRouterProvider.otherwise("/");
 
-module.controller("homeCtrl", function($scope, $rootScope, kkService){
-    var promise = kkService.getRecipe();
-    promise.then(function(data){
-        $scope.recipes = data;     
-        console.log($scope.recipes);
+    $stateProvider.state("home", {
+        url: "/",
+        templateUrl: "templates/home.html",
+        controller: "homeCtrl"
     });
 });
 
-module.service("kkService", function ($q,$http,$rootScope){
-    this.getRecipe = function(){
+module.controller("headerCtrl", function ($scope, $rootScope, kkService) {
+    $scope.submitForm = function () {
+        console.log($scope.username);
+        console.log($scope.password);
+        kkService.checkLogin().then(function () {
+            $rootScope.username = $scope.username;
+            $rootScope.password = $scope.password;
+        }).error(function () {
+            console.log("Error i submitForm");
+        });
+    };
+});
+
+module.controller("homeCtrl", function ($scope, $rootScope, kkService) {
+    var promise = kkService.getRecipe();
+    promise.then(function (data) {
+        $scope.recipes = data.data;
+        console.log(data.data);
+    });
+});
+
+module.service("kkService", function ($q, $http, $rootScope) {
+    this.getRecipe = function () {
         var deffer = $q.defer();
         var url = "http://localhost:8080/KKApp/webresources/recept";
         var auth = "Basic Q2FzcGVyOmphZ2VlYWRtaW4=";
@@ -27,9 +40,13 @@ module.service("kkService", function ($q,$http,$rootScope){
             url: url,
             method: "GET",
             headers: {'Authorization': auth}
-        }).then(function (data){
+        }).then(function (data) {
             deffer.resolve(data);
         });
         return deffer.promise;
+    };
+    
+    this.checkLogin = function(){
+        
     };
 });
